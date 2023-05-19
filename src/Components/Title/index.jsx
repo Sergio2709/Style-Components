4cd4662e19ca27c645dc072c04a7dfcbeed10fcd
Sergio Castillo <serggcastillo@gmail.com>
2023-05-18 19:25:41 -0500

    First version

---
+++ src/Components/Title/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: grey;
+  padding: 25px 0;
+`
+
+const suma =  1 + 1 ;
+console.log(suma)
+
+//const Title = ({ children }) => {
+  //return <h1 className="title">{children}</h1>;
+//};
+export default Title;
